0f5ef4ebb143ef26a122aee9aa1e8a3a985895a8
Bernardo Busatto <berbusatto@gmail.com>
2023-11-23 22:38:42 -0300

    add: delete user funcionando, tratamento de exceptions

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,29 +3,52 @@ import { UserService } from '../services/UserService';
 
 export class UserController {
     createUser = (request: Request, response: Response) => {
+        try {
             const userService = new UserService();
             const user = request.body;
+    
+            if (!user || !user.name || !user.email || !user.id) {
+                throw new Error('Bad request: invalid user data');
+            }  
 
-            if(!user.name){
-                return response.status(400).json({message: 'Bad request: invalid name'}); 
-            }
-            userService.createUser(user.name, user.email);
-            
-            return response.status(201).json({ message: 'User created' });        
+            userService.createUser(user.name, user.email, user.id);
+    
+            return response.status(201).json({ message: 'User created' });
+        } catch (error: any) {
+            return response.status(400).json({ message: error.message });
         }
+    }
 
 
     getAllUsers = (request: Request, response: Response) => {
-        const userService = new UserService();
-        const users = userService.getAllUsers();
-        return response.status(200).json(users);
+        try {
+            const userService = new UserService();
+            const users = userService.getAllUsers();
+
+            return response.status(200).json(users);
+        } catch (error: any) {
+            return response.status(500).json({ message: 'Internal Server Error', error: error.message });
+        }
     }
 
     deleteUser = (request: Request, response: Response) => {
-        const user = request.body;
-        console.log(user);
-        // sem implementação na service por enquanto...
-        return response.status(200).json({message: 'User deleted'})
+        try {
+            const userIdParam = request.query.id;
+            if (!userIdParam) {
+                throw new Error('Missing userId in query parameters');
+            }
+    
+            const userId = parseInt(userIdParam as string, 10);
+            if (isNaN(userId)) {
+                throw new Error('Invalid userId format');
+            }
+    
+            const userService = new UserService();
+            userService.deleteUser(userId);
+    
+            return response.status(200).json({ message: 'User deleted' });
+        } catch (error: any) {
+            return response.status(404).json({ message: error.message });
+        }
     }
-
-    }
+}
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,22 +1,41 @@
 const db = [
     {
         name: "Bernardo",
-        email: "bernardo@dio.com"
-    }
+        email: "bernardo@dio.com",
+        id: 1
+    },
+    {
+        name: "Maria",
+        email: "maclau@dio.com",
+        id: 2
+    },
+
 ]
 
 
 export class UserService {
-    createUser = (name: string, email: string) => {
+    createUser = (name: string, email: string, id: number) => {
         const user = {
-            name, 
-            email
+            name,
+            email,
+            id
         }
         db.push(user);
-        console.log('DB updated', db);                
+        console.log('DB updated', db);
     }
 
     getAllUsers = () => {
         return db;
+    }
+
+    deleteUser = (id: number) => {
+        const userIndex = db.findIndex((user) => user.id === id);
+
+        if (userIndex === -1) {
+            throw new Error('User not found');
+        }
+
+        db.splice(userIndex, 1);
+
     }
 }
